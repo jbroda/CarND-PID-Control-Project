6539a2fac2f0c1df875b01aea191fa00f35e4879
jbroda <jbroda@gmail.com>
2017-06-08 16:15:10 -0500

    Clean up.

--- src/main.cpp
+++ src/main.cpp
@@ -65,28 +65,9 @@ int main()
   bool bTwiddle = false;
 
   // TODO: Initialize the pid variable.
-  // I used the parameters given in the lesson.
   pid.Init(0.2, 0.004, 3.00);
-  //pid.Init(0.0, 0.000, 0.00);
-  //pid.Init(0.559343, 7.89516e-06, 11.8724);
-  //pid.Init(0.16, 0.002, 20.0);
-  //pid.Init(0.2, 0, 20);
 
-  std::vector<double> dp = { 1, 1, 1 };
-  double * p[] = { &pid.Kp, &pid.Kd, &pid.Ki };
-  unsigned int iRun = 0;
-  unsigned int iDpIndex = 0;
-  unsigned int iTwiddlePhase = 0;
-  double err = 0.0;
-  double err_sum = 0.0;
-  double best_err = 1e9;
-  const unsigned int N = 100;
-
-  std::cout << "Initial Kp: " << pid.Kp << " Kd: " << pid.Kd << " Ki: " << pid.Ki << std::endl;
-
-  h.onMessage([&bTwiddle, 
-	  &pid, &dp, &p, &iRun, &iDpIndex, &iTwiddlePhase, 
-	  &err, &err_sum, &best_err, &N](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -113,149 +94,9 @@ int main()
 
 		  pid.UpdateError(cte);
 
-		  /*
-		  if (cte > 5)
-		  {
-			  iRun = 0;
-			  err_sum = 0;
-			  pid.ResetError();
-			  reset_sim(ws);
-			  bTwiddle = true;
-		  }
-		  */
-
 		  steer_value = pid.SteeringValue();
 
-		  /*
-		  if (iRun >= N)
-		  {
-			  err_sum += pow(cte, 2);
-		  }
-
-		  if (iRun >= 2*N)
-		  {
-			  err = err_sum / N;
-
-			  cout << "run: " << iRun << " ERR: " << err << endl;
-
-			  err_sum = 0;
-
-			  iRun = 0;
-
-			  // bTwiddle = true;
-		  }
-		  else
-		  {
-			  ++iRun;
-		  }
-
-		  //
-		  // Twiddle.
-		  //
-		  if (bTwiddle)
-		  {
-			  double sum = std::accumulate(dp.begin(), dp.end(), 0.0);
-
-			  if (sum > 0.2)
-			  {
-				  bool bReset = false;
-
-				  std::cout << "Phase: " << iTwiddlePhase << " iDpIndex: " << iDpIndex << " BEST ERR: " << best_err << " ERR: " << err << " sum: " << sum << std::endl;
-
-				  switch (iTwiddlePhase)
-				  {
-				  case 0:
-				  {
-					  *p[iDpIndex] += dp[iDpIndex];
-
-					  ++iTwiddlePhase;
-
-					  bReset = true;
-
-					  break;
-				  }
-
-				  case 1:
-				  {
-					  if (err < best_err)
-					  {
-						  best_err = err;
-
-						  dp[iDpIndex] *= 1.1;
-
-						  iTwiddlePhase = 0;
-
-						  ++iDpIndex;
-						  iDpIndex %= 3;
-					  }
-					  else
-					  {
-						  *p[iDpIndex] -= 2 * dp[iDpIndex];
-
-						  ++iTwiddlePhase;
-
-						  bReset = true;
-					  }
-
-					  break;
-				  }
-
-				  case 2:
-				  {
-					  if (err < best_err)
-					  {
-						  best_err = err;
-						  dp[iDpIndex] = 1.1;
-					  }
-					  else
-					  {
-						  *p[iDpIndex] += dp[iDpIndex];
-						  dp[iDpIndex] *= 0.9;
-					  }
-
-					  iTwiddlePhase = 0;
-
-					  ++iDpIndex;
-					  iDpIndex %= 3;
-
-					  break;
-				  }
-
-				  default:
-				  {
-					  assert(false);
-					  break;
-				  }
-				  }
-
-				  std::cout << "New Kp: " << pid.Kp << " Kd: " << pid.Kd << " Ki: " << pid.Ki << std::endl;
-
-				  // Reset simulator.
-				  if (bReset)
-				  {
-					  iRun = 0;
-					  err_sum = 0;
-					  bTwiddle = false;
-					  pid.ResetError();
-					  reset_sim(ws);
-					  return;
-				  }
-			  }
-			  else
-			  {
-				  bTwiddle = false;
-			  }
-		  } 
-		  */
-
-		  //steer_value = fmax(-1.0, steer_value);
-		  //steer_value = fmin(1.0, steer_value);
-
-		  double K = 2.0;
-		  double throttle = K * (60 - speed) - pid.Kp*pid.p_error - pid.Kd * pid.d_error - pid.Ki * pid.i_error;
-		  throttle = 0.3;
-		  // # The throttle speed is managed according to the steering angle.
-		  // throttle = max(0.1, -0.15/0.05 * abs(steering_angle) + 0.35)
+		  double throttle = 0.3;
 
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << " Throttle: " << throttle << std::endl;
